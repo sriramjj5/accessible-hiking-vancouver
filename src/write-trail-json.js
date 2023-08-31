34e09ed6262830b7a2bdedeb3ff4c03ec9386dd2
Sriram <sriramjj5@gmail.com>
2023-08-31 14:26:42 -0700

    Remove unnecessary fs import

--- src/write-trail-json.js
+++ src/write-trail-json.js
@@ -1,7 +1,5 @@
 import { getTrailCoordinates } from "./get-trail-coordinates";
 
-const fs = require('fs');
-
 export async function getTrailJson() {
     const trails = await getTrailCoordinates();
     const trailJson = JSON.stringify(trails);
